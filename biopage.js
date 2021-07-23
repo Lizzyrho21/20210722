13d3dbcd156d5e47cfa2387771d16c1e5546f687
Lizzy Wiley <Lizzyrho@gmail.com>
2021-07-23 17:35:00 -0500

    Completed JS and CSS steps

--- biopage.js
+++ biopage.js
@@ -1,6 +1,25 @@
-  // We want to write a function that nests the qeustion and runs it when the page loads.
+// We want to write a function that nests the qeustion and runs it when the page loads.
 
-      // this is the first question in the functions
+
+
+
+
+var totalScore = "0"; // I could not figure out how to turn this into a function.
+
+
+  
+  
+
+   
+
+  
+
+  
+
+
+
+
+
 
       let userName = prompt("Greetings! What is your name?"); //declare a variable and ask the user a question
       let confirmGuess = confirm(
@@ -13,8 +32,10 @@
           let response = prompt("Am I 22? y/n");
 
           if (response === "y" || response.toUpperCase() === "YES") {
+            totalScore++;
             alert("You got it!!");
             rightAnswer = true;
+            
           } else {
             alert("No, try again..");
           }
@@ -22,21 +43,23 @@
       }
 
       ageCheck();
+      
+      
 
-      /* we want this loop question inside of a function so that 
+      /* we want this loop question inside of a function so that
  when the function is called the loop will begin!
 
- 1. We want the loop inside of a function 
- what is a function?
- 
- 
+  We want the loop inside of a function
+
+
 */
 
       function answerForDoIPlay() {
         let rightAnswer = false;
         while (rightAnswer === false) {
           let response = prompt("Do I like to play music? y/n? ");
-          if (response === "y" || response === "yes") {
+          if (response === 'yes' || response.toUpperCase() === 'YES') {
+            totalScore++;
             rightAnswer = true;
             alert("You got it!!");
           } else {
@@ -44,7 +67,6 @@
           }
         }
       }
-     
 
       answerForDoIPlay();
 
@@ -53,7 +75,8 @@
         while (cornBread === false) {
           let response = prompt("Do I like cornbread? y/n?");
 
-          if (response === "n" || response === "no") {
+          if (response === "no" || response.toUpperCase() === "NO") {
+            totalScore++;
             alert("You guessed it!");
             cornBread = true;
           } else {
@@ -68,7 +91,8 @@
         while (loveToDraw === false) {
           let response = prompt("Do I love to draw?");
 
-          if (response === "yes" || response == "y") {
+          if (response === "yes" || response.toUpperCase() == "YES") {
+            totalScore++;
             alert("AWESOME!!");
             loveToDraw = true;
           } else {
@@ -86,9 +110,12 @@
             "Last y / n  " + userName + " , Do you think you'll like my page?"
           );
 
-          if (response === "y" || response === "yes") {
+          if (response === "yes" || response.toUpperCase() === "Y") {
+            totalScore++;
             alert(userName + " , You guessed correctly!!");
+            
             likePage = true;
+            
           } else {
             alert(" ...Let's try this again, " + userName);
           }
@@ -96,6 +123,7 @@
       }
 
       doYouLikeMyPage();
+      
 
       function fourAttemptQuestions() {
         for (attempts = 1; attempts <= 4; attempts++) {
@@ -109,39 +137,69 @@
             } else if (ageEntered < correctAge) {
               alert("I'm not THAT young..");
             } else if (ageEntered === correctAge) {
+              totalScore++;
+              
               alert("You got it!");
               rightAnswer = false;
+              
             }
           }
           break;
         }
       }
 
-      fourAttemptQuestions ();
+fourAttemptQuestions ();
 
-      function checkTheAnswers() {
-        let studyHours = ["2", "4", "6", "8", "10"];
 
-        for (answer = 1; answer <= 6; answer++) {
-          let studyHourResponse = prompt(
-            "How many hours do you think I study?"
-          );
 
-          for (i = 0; i < studyHours.length; i++) {
-            // console.log(studyHours[i]);
+function checkTheAnswers() {
+  let studyHours = ["2", "4", "6", "8", "10"]; //declaring our variable for the correct answers
 
-            if (studyHourResponse === studyHours[i]) {
-              alert("That's right! Guess again?");
-            }
-            
-           
-          }
-          
-    }
-   
+  for (answer = 1; answer <= 6; answer++) {
+    /* Make the question loop six times*/
+    let studyHourResponse = prompt(
+      "How many hours do you think I study?" //ask the user a question
+    );
+      let answerFound = false;
+    for (i = 0; i < studyHours.length; i++) {
+      /* Here, we are making a loop that will be the next correct answer in the array for the user to input*/
+      // console.log(studyHours[i]);
+
+      if (studyHourResponse === studyHours[i]) {
+        // If the user answer correctly, the first 'for' loop will run again
+        totalScore++
+        alert("That's right! Guess again?");
+        answerFound = true;
         
-        let finalAnswer = alert("Cool! the correct answers were " + studyHours + " Enjoy!!");
+        break;
       }
-      checkTheAnswers();
+      
+    }
+    if (answerFound === false) {
+      //If the user answer incorrectly, The first 'for' oop will run again.
+      alert("Umm noo..");
+    }
+  }
 
-    
+  let finalAnswer = alert(
+    "Cool! the correct answers were " + studyHours + " Enjoy!!"
+  );
+}
+checkTheAnswers();
+alert("Your total score is " + totalScore);
+console.log(totalScore);
+
+
+
+//let userEndScore = alert("You got " + scoreKeeper());
+
+
+
+/* We want a question that has multiple answers,
+
+      We want to give the user 6 attempts to answer the the question
+      Eeverytime a user gets an answer right, they will get a message saying they got it right
+      the question gets asked to the user again
+      If the user gets the answer wrong, we want to let the user know the answer was wrong.
+      The question gets asked a total of six times.
+      After the attempts are up, the user will get a message revealing the right answers. */
